b2dc80649e789778e3709eacc0f63457c5e53e71
BenPet <bendzuke@gmail.com>
2023-12-07 21:17:11 +0200

    Added QuerySelectors

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-/* This is the Main*/
+const grid = document.querySelector(".grid")
+const overlay = document.querySelector(".overlay");
+let playAgain = document.querySelector(".playAgain");
+const scoreDisplay = document.querySelector(".scoreDisplay")
+const startButton = document.querySelector(".startButton")
