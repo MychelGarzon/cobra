af2064e85a092df72f2476770c870fd05b7bc28d
Mychel Garzon <mtjomy7@gmail.com>
2023-12-07 21:33:08 +0200

    MG buildBoard and startGame functions

--- main.js
+++ main.js
@@ -3,3 +3,32 @@ const overlay = document.querySelector(".overlay");
 let playAgain = document.querySelector(".playAgain");
 const scoreDisplay = document.querySelector(".scoreDisplay")
 const startButton = document.querySelector(".startButton")
+
+
+/* Variables*/
+let width = 10;
+let direction = 1
+let cobraArray = [2, 1, 0]
+/*Variables*/
+
+
+const buildBoard = () => {
+    for (let index = 0; index < 100; index++) {
+        let div = document.createElement("div")
+        grid.appendChild(div)
+    }
+}
+
+
+const startGame = () => {
+
+    let boxes = document.querySelectorAll(".grid div")
+    direction = 1
+    scoreDisplay.innerHTML = score
+    timeBetweenMovements = 1000
+    cobraArray = [2, 1, 0]
+    cobraPosition = 0
+    cobraArray.forEach(index => boxes[index].classList.add("cobra"))
+
+
+}
